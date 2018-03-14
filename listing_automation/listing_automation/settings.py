bae8504f144e9165b89aa15a0a4d69c62305d5a1
root <saurabhchopra0108@gmail.com>
2018-03-14 10:33:32 +0530

    errorCommit

--- listing_automation/listing_automation/settings.py
+++ listing_automation/listing_automation/settings.py
@@ -122,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+FILE_UPLOAD_HANDLERS = ("django_excel.ExcelMemoryFileUploadHandler",
+                        "django_excel.TemporaryExcelFileUploadHandler")
--- listing_automation/listing_automation/views.py
+++ listing_automation/listing_automation/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+import logging
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 def upload_csv(request):
     data = {}
@@ -9,11 +13,11 @@ def upload_csv(request):
         csv_file = request.FILES["csv_file"]
         if not csv_file.name.endswith('.csv'):
             messages.error(request,'File is not CSV type')
-            return HttpResponseRedirect(reverse("myapp:upload_csv"))
+            return HttpResponseRedirect(reverse("upload_csv"))
         #if file is too large, return
         if csv_file.multiple_chunks():
             messages.error(request,"Uploaded file is too big (%.2f MB)." % (csv_file.size/(1000*1000),))
-            return HttpResponseRedirect(reverse("myapp:upload_csv"))
+            return HttpResponseRedirect(reverse("upload_csv"))
 
         file_data = csv_file.read().decode("utf-8")
 
@@ -40,4 +44,4 @@ def upload_csv(request):
         logging.getLogger("error_logger").error("Unable to upload file. "+repr(e))
         messages.error(request,"Unable to upload file. "+repr(e))
 
-    return HttpResponseRedirect(reverse("myapp:upload_csv"))
+    return HttpResponseRedirect(reverse("upload_csv"))
